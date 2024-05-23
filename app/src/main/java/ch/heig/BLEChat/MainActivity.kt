bd1230d14a7ef265cd955f21dc9b8533036d45c2
Kylian <42773865+kylianmanzini@users.noreply.github.com>
2024-05-23 12:12:39 +0200

    visual for base chatapp

--- app/src/main/java/ch/heig/BLEChat/MainActivity.kt
+++ app/src/main/java/ch/heig/BLEChat/MainActivity.kt
@@ -1,20 +1,39 @@
 package ch.heig.BLEChat
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
+    private var recyclerViewMessages: RecyclerView? = null
+    private var editTextMessage: EditText? = null
+    private var buttonSend: Button? = null
+    private var messageList: MutableList<String> = ArrayList()
+    private var messageAdapter: MessageAdapter = MessageAdapter(messageList)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        recyclerViewMessages = findViewById<RecyclerView>(R.id.recyclerViewMessages)
+        editTextMessage = findViewById<EditText>(R.id.editTextMessage)
+        buttonSend = findViewById<Button>(R.id.buttonSend)
+
+        recyclerViewMessages?.setLayoutManager(LinearLayoutManager(this))
+        recyclerViewMessages?.setAdapter(messageAdapter)
+        buttonSend?.setOnClickListener(View.OnClickListener {
+            val message = editTextMessage?.getText().toString().trim { it <= ' ' }
+            if (message.isNotEmpty()) {
+                messageList.add(message)
+                messageAdapter.notifyItemInserted(messageList.size - 1)
+                recyclerViewMessages?.scrollToPosition(messageList.size - 1)
+                editTextMessage?.setText("")
+            }
+        })
     }
 }
+
---
+++ app/src/main/java/ch/heig/BLEChat/MessageAdapter.kt
@@ -0,0 +1,34 @@
+package ch.heig.BLEChat
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import ch.heig.BLEChat.MessageAdapter.MessageViewHolder
+
+class MessageAdapter(private val messageList: List<String>) :
+    RecyclerView.Adapter<MessageViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_message, parent, false)
+        return MessageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+        val message = messageList[position]
+        holder.textViewMessage.text = message
+    }
+
+    override fun getItemCount(): Int {
+        return messageList.size
+    }
+
+    class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var textViewMessage: TextView
+
+        init {
+            textViewMessage = itemView.findViewById(R.id.textViewMessage)
+        }
+    }
+}
